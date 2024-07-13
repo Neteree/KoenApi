c14150124cc2b9cf473b7f12bbc72a70342352fa
Neteree <neteree@gmail.com>
2024-07-13 18:48:08 +1200

    Rewrote koen_csv.lua so that it now deals with invalid entires in the csv and now you can see if you have have missed an entry for Korean, English or both Korean and English. Additionally now empty/unfilled lines don't result in unwanted tables being produced

--- koen_csv.lua
+++ koen_csv.lua
@@ -1,13 +1,31 @@
 M = {}
 
--- This function was just ment to be a proof of concept, so no
--- checks/data validation was done
 function M.to_word_pairs()
   local word_pairs = {}
   
   for line in io.lines("koen.csv") do
-    local ko, en = line:match("%s*(.-),%s*(.*)")
-    table.insert(word_pairs, {ko = ko, en = en})
+    line = line:gsub("%s+", "")
+
+    if #line > 0 then
+      local ko, en = line:match("%s*(.-),%s*(.*)")
+      
+      if not ko then
+        ko = line
+      end
+      
+      if #ko == 0 then 
+        ko = "Korean word missing"
+      end
+
+      if not en or #en == 0 then
+        en = "English word missing"
+      end
+       
+      table.insert(word_pairs, {
+        ko = ko,
+	en = en
+      })
+    end
   end
 
   return word_pairs
